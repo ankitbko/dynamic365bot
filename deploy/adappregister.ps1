ed80f29c9ab7cb165435ee0a28f14501a892ac41
Ankit Sinha <ankitbko@gmail.com>
2018-04-20 17:34:17 +0530

    added deployment script

---
+++ deploy/adappregister.ps1
@@ -0,0 +1,178 @@
+<# This script will create a single Azure AD Application in your tenant, apply the appropriate permissions to it and execute a test call against a specified endpoint. Modify the values at the top of this script as required. #>
+
+#https://gcits.com/knowledge-base/automate-api-calls-microsoft-graph-using-powershell-azure-active-directory-applications/
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $applicationName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $appIdUri
+)
+  
+# Modify the homePage, appIdURI and logoutURI values to whatever valid URI you like. They don't need to be actual addresses.
+$homePage = $appIdURI
+$logoutURI = $appIdURI
+
+# Enter the required permissions below, separated by spaces eg: "Directory.Read.All Reports.Read.All Group.ReadWrite.All Directory.ReadWrite.All"
+#$ApplicationPermissions = "Reports.Read.All"
+$ApplicationPermissions = $null
+  
+# Set DelegatePermissions to $null if you only require application permissions. 
+$DelegatedPermissions = "user_impersonation"
+# Otherwise, include the required delegated permissions below.
+# $DelegatedPermissions = "Directory.Read.All Group.ReadWrite.All"
+  
+  
+Function AddResourcePermission($requiredAccess, $exposedPermissions, $requiredAccesses, $permissionType) {
+    foreach ($permission in $requiredAccesses.Trim().Split(" ")) {
+        $reqPermission = $null
+        $reqPermission = $exposedPermissions | Where-Object {$_.Value -contains $permission}
+        Write-Host "Collected information for $($reqPermission.Value) of type $permissionType" -ForegroundColor Green
+        $resourceAccess = New-Object Microsoft.Open.AzureAD.Model.ResourceAccess
+        $resourceAccess.Type = $permissionType
+        $resourceAccess.Id = $reqPermission.Id    
+        $requiredAccess.ResourceAccess.Add($resourceAccess)
+    }
+}
+  
+Function GetRequiredPermissions($requiredDelegatedPermissions, $requiredApplicationPermissions, $reqsp) {
+    $sp = $reqsp
+    $appid = $sp.AppId
+    $requiredAccess = New-Object Microsoft.Open.AzureAD.Model.RequiredResourceAccess
+    $requiredAccess.ResourceAppId = $appid
+    $requiredAccess.ResourceAccess = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.ResourceAccess]
+    if ($requiredDelegatedPermissions) {
+        AddResourcePermission $requiredAccess -exposedPermissions $sp.Oauth2Permissions -requiredAccesses $requiredDelegatedPermissions -permissionType "Scope"
+    } 
+    if ($requiredApplicationPermissions) {
+        AddResourcePermission $requiredAccess -exposedPermissions $sp.AppRoles -requiredAccesses $requiredApplicationPermissions -permissionType "Role"
+    }
+    return $requiredAccess
+}
+  
+Function GenerateAppKey ($fromDate, $durationInYears, $pw) {
+    $endDate = $fromDate.AddYears($durationInYears) 
+    $keyId = (New-Guid).ToString();
+    $key = New-Object Microsoft.Open.AzureAD.Model.PasswordCredential($null, $endDate, $keyId, $fromDate, $pw)
+    return $key
+}
+  
+Function CreateAppKey($fromDate, $durationInYears, $pw) {
+  
+    $testKey = GenerateAppKey -fromDate $fromDate -durationInYears $durationInYears -pw $pw
+  
+    while ($testKey.Value -match "\+" -or $testKey.Value -match "/") {
+        Write-Host "Secret contains + or / and may not authenticate correctly. Regenerating..." -ForegroundColor Yellow
+        $pw = ComputePassword
+        $testKey = GenerateAppKey -fromDate $fromDate -durationInYears $durationInYears -pw $pw
+    }
+    Write-Host "Secret doesn't contain + or /. Continuing..." -ForegroundColor Green
+    $key = $testKey
+  
+    return $key
+}
+  
+Function ComputePassword {
+    $aesManaged = New-Object "System.Security.Cryptography.AesManaged"
+    $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+    $aesManaged.BlockSize = 128
+    $aesManaged.KeySize = 256
+    $aesManaged.GenerateKey()
+    return [System.Convert]::ToBase64String($aesManaged.Key)
+}
+  
+function GetOrCreateMicrosoftGraphServicePrincipal {
+    $graphsp = Get-AzureADServicePrincipal -SearchString "Microsoft.CRM"
+    $graphsp = $graphsp[0]
+    if (!$graphsp) {
+        $graphsp = Get-AzureADServicePrincipal -SearchString "Microsoft.Azure.AgregatorService"
+    }
+    if (!$graphsp) {
+        Login-AzureAccount
+        New-AzureRmADServicePrincipal -ApplicationId "00000003-0000-0000-c000-000000000000"
+        $graphsp = Get-AzureADServicePrincipal -SearchString "Microsoft Graph"
+    }
+  
+    return $graphsp
+}
+  
+Connect-AzureAD
+Write-Host (Get-AzureADTenantDetail).displayName
+  
+# Check for a Microsoft Graph Service Principal. If it doesn't exist already, create it.
+$graphsp = GetOrCreateMicrosoftGraphServicePrincipal
+  
+$existingapp = $null
+$existingapp = get-azureadapplication -SearchString $applicationName
+if ($existingapp) {
+    Remove-Azureadapplication -ObjectId $existingApp.objectId
+}
+ 
+$rsps = @()
+if ($graphsp) {
+    $rsps += $graphsp
+    $tenant_id = (Get-AzureADTenantDetail).ObjectId
+    $tenantName = (Get-AzureADTenantDetail).DisplayName
+    $azureadsp = Get-AzureADServicePrincipal -SearchString "Windows Azure Active Directory"
+    $rsps += $azureadsp
+  
+    # Add Required Resources Access (Microsoft Graph)
+    $requiredResourcesAccess = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.RequiredResourceAccess]
+    $microsoftGraphRequiredPermissions = GetRequiredPermissions -reqsp $graphsp -requiredApplicationPermissions $ApplicationPermissions -requiredDelegatedPermissions $DelegatedPermissions
+    $requiredResourcesAccess.Add($microsoftGraphRequiredPermissions)
+  
+    if ($DelegatedPermissions) {
+        Write-Host "Delegated Permissions specified, preparing permissions for Azure AD Graph API"
+        # Add Required Resources Access (Azure AD Graph)
+        #$AzureADGraphRequiredPermissions = GetRequiredPermissions -reqsp $azureadsp -requiredApplicationPermissions "Directory.ReadWrite.All"
+        $AzureADGraphRequiredPermissions = GetRequiredPermissions -reqsp $azureadsp -requiredDelegatedPermissions "User.Read"
+        $requiredResourcesAccess.Add($AzureADGraphRequiredPermissions)
+    }
+  
+  
+    # Get an application key
+    $pw = ComputePassword
+    $fromDate = [System.DateTime]::Now
+    $appKey = CreateAppKey -fromDate $fromDate -durationInYears 2 -pw $pw
+  
+    Write-Host "Creating the AAD application $applicationName" -ForegroundColor Blue
+
+    try 
+    {
+    $aadApplication = New-AzureADApplication -DisplayName $applicationName `
+        -HomePage $homePage `
+        -ReplyUrls $homePage `
+        -IdentifierUris $appIdURI `
+        -LogoutUrl $logoutURI `
+        -RequiredResourceAccess $requiredResourcesAccess `
+        -PasswordCredentials $appKey
+
+    }
+    catch [Exception] {
+        echo $_.Exception|format-list -force
+    }
+
+    Write-Host "App Created" -ForegroundColor Green
+      
+    # Creating the Service Principal for the application
+    $servicePrincipal = New-AzureADServicePrincipal -AppId $aadApplication.AppId
+      
+    Write-Host "Service Principle Created" -ForegroundColor Green
+
+    Write-Host "Application ID: $aadApplication.AppId" -ForegroundColor DarkYellow
+    Write-Host "Application Secret: $appkey.Value" -ForegroundColor DarkYellow
+    $tenant_id = (Get-AzureADTenantDetail).ObjectId
+    Write-Host "Tenant ID: $tenant_id" -ForegroundColor DarkYellow   
+
+	[hashtable]$Return = @{} 
+	$Return.appId = [string]$aadApplication.AppId
+	$Return.appSecret = [string]$appkey.Value
+	Return $Return 
+}
+else {
+    Write-Host "Microsoft Graph Service Principal could not be found or created" -ForegroundColor Red
+}
---
+++ deploy/deploy.ps1
@@ -0,0 +1,133 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+
+ .DESCRIPTION
+    Deploys an Azure Resource Manager template
+
+ .PARAMETER subscriptionId
+    The subscription id where the template will be deployed.
+
+ .PARAMETER resourceGroupName
+    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
+
+ .PARAMETER resourceGroupLocation
+    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
+
+ .PARAMETER deploymentName
+    The deployment name.
+
+ .PARAMETER templateFilePath
+    Optional, path to the template file. Defaults to template.json.
+
+ .PARAMETER parametersFilePath
+    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+#>
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $subscriptionId,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $resourceGroupName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $resourceGroupLocation,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+ 
+ [Parameter(Mandatory=$True)]
+ [string]
+ $adAppName,
+ 
+ [Parameter(Mandatory=$True)]
+ [object]
+ $templateParameters
+)
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+if(!(Get-PackageProvider -Name Nuget) -Or ((Get-PackageProvider -Name Nuget).Version -lt [System.Version]"2.8.5.208")) {
+	Write-Host "Nuget does not exist or version is less than required" -ForegroundColor DarkYellow
+	Install-PackageProvider -Name Nuget -MinimumVersion 2.8.5.208 -Scope CurrentUser -Force
+    Write-Host "Nuget Installed" -ForegroundColor DarkYellow
+}
+
+if(!(Get-PSRepository -Name "PSGallery")) {
+    Write-Host "Setting PSGallery as repository" -ForegroundColor DarkYellow
+	Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
+
+if(!(Get-Module -Listavailable -Name AzureRM)) {
+    Write-Host "Installing AzureRM" -ForegroundColor DarkYellow
+	Install-Module -Name AzureRM -AllowClobber
+}
+
+if(!(Get-Module -Listavailable -Name AzureRM.Profile)) {
+    Write-Host "Installing AzureRM.Profile" -ForegroundColor DarkYellow
+	Install-Module -Name AzureRM.Profile -AllowClobber
+}
+
+if(!(Get-Module -Listavailable -Name AzureAD)) {
+    Write-Host "Installing AzureAD" -ForegroundColor DarkYellow
+	Install-Module -Name AzureAD -AllowClobber
+}
+
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+
+# Deploy Resources
+	$deploymentResult = & "$ScriptPath\deployArm.ps1" $subscriptionId $resourceGroupName $resourceGroupLocation $deploymentName $templateParameters
+
+Write-Host "Deployment Done. Deployment Result:" -ForegroundColor Green
+Write-Host ($deploymentResult | Out-String) -ForegroundColor Green
+
+$luisSubscriptionKey = $deploymentResult.Outputs.luisKey.Value
+$botSiteObject = $deploymentResult.Outputs.item("botSite").Value.ToString() | ConvertFrom-Json
+$botHostName = $botSiteObject.properties.defaultHostname
+$botCallbackUrl = "https://$botHostName/api/OAuthCallback"
+
+# Register AzureAD V1 App.
+$adAppResult = & "$ScriptPath\adappregister.ps1" $adAppName $botCallbackUrl
+
+Write-Host "Azure App created"
+Write-Host ($adAppResult | Out-String) -ForegroundColor Green
+
+# Update WebApp Config
+Write-Host "Trying to update AppSettings"
+$botApp = Get-AzureRmWebapp -Name $botSiteObject.properties.name -ResourceGroup $botSiteObject.resourceGroupName
+$AppSettings =	@{
+	"ActiveDirectory.RedirectUri" = "$botCallbackUrl";
+	"ActiveDirectory.ClientId" = "$($adAppResult.appId)";
+	"ActiveDirectory.ClientSecret" = "$($adAppResult.appSecret)" 
+}
+foreach ($pair in $botApp.SiteConfig.AppSettings) { $AppSettings[$pair.Name] = $pair.Value }
+
+Set-AzureRmWebapp -Name $botSiteObject.properties.name -ResourceGroup $botSiteObject.resourceGroupName -AppSettings $AppSettings
+
+Write-Host "Appsettings Updated"
+Write-Host ($AppSettings | Out-String) -ForegroundColor Green
+
+# Create a App Insight API key and set it to Bot Registration
+Write-Host "Generating AppInsight API Key"
+$apiKeyDescription="DynamicsBotkey"
+$permissions = @("ReadTelemetry", "WriteAnnotations")
+$appInsightName = $deploymentResult.Outputs.appInsightName.Value
+$appInsightApiKey = New-AzureRmApplicationInsightsApiKey -ResourceGroupName $resourceGroupName -Name $appInsightName -Description $apiKeyDescription -Permissions $permissions
+Write-Host ($appInsightApiKey | Out-String) -ForegroundColor Green
+
+Write-Host "Setting AppInsight API Key in Bot Registration"
+$botRegistrationName = $deploymentResult.Outputs.botRegistrationName.Value
+$botRegistration = Get-AzureRmResource -Name $botRegistrationName -ResourceGroup $resourceGroupName -ExpandProperties
+$botRegistration.Properties | Add-Member developerAppInsightsApiKey $appInsightApiKey.ApiKey
+Write-Host "New Bot Registration Property: $($botRegistration.Properties | Out-String)" -ForegroundColor Green
+$botRegistration | Set-AzureRmResource -Force
+Write-Host "AppInsight API Key Set"
+
+Write-Host "---------------------------------" -ForegroundColor Green
+Write-Host "LUIS Key: $luisSubscriptionKey" -ForegroundColor Green
+Write-Host "---------------------------------" -ForegroundColor Green
